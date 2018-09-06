51b10aabd33ee471ebc94c0cfffc216eec635744
Samiurrahman Shaikh <Samiurrahman.Shaikh@morningstar.com>
2018-09-06 15:30:22 +0530

    'setupDone'

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,7 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/users');
-//Server config
+//Server config files
 // mongoose.connect(config.prodURL);
 mongoose.Promise = global.Promise;
 
