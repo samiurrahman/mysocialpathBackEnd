e98125e62ff06ef8bdd9f8e08bdf50819a8e3f76
Samiurrahman Shaikh <Samiurrahman.Shaikh@morningstar.com>
2018-10-26 15:03:53 +0530

    update

--- users/user.service.js
+++ users/user.service.js
@@ -62,6 +62,9 @@ async function getAll(username) {
                 },
                 { 
                     'lastName':  {'$regex' : username} 
+                },
+                { 
+                    'email':  {'$regex' : username} 
                 }
             ]
         }).select('-hash');
